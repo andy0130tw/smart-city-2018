9de1e19f0e3f1d696d1672512068cf0b6be55f7c
N.M <freedomtomdestiny@gmail.com>
2018-10-06 13:20:40 +0800

    Cleaning some script

--- static/js/index.js
+++ static/js/index.js
@@ -19,14 +19,17 @@ import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
 var map = L.map('map', {
-  minZoom: 10,
-  maxZoom: 20,
-  maxBounds: [[24.5, 120], [25.5, 123]]
+    minZoom: 10,
+    maxZoom: 20,
+    maxBounds: [
+        [24.5, 120],
+        [25.5, 123]
+    ]
 }).setView([25.046401, 121.517641], 12);
 
 L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
 }).addTo(map);
 
 // L.marker([25.046401, 121.517641]).addTo(map)
@@ -34,39 +37,39 @@ L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
 //     .openPopup();
 
 function parseCSV(csv) {
-  let clusterGroup = new L.MarkerClusterGroup({
-    disableClusteringAtZoom: 15,
-    maxClusterRadius: zoom => {
-      if (zoom <= 11) return 80;
-      if (zoom <= 12) return 70;
-      if (zoom <= 14) return 60;
-      return 50;
-    }
-  });
-
-  let markers = [];
-
-  csv
-    .split('\n').slice(1)
-    .map(line => line.split(','))
-    .forEach(arr => {
-      let [sno, sna, tot, lat, lng, sarea] = arr;
-
-      let mark = L.ExtraMarkers.icon({
-        icon: 'fa-bicycle',
-        markerColor: 'blue',
-        shape: 'square',
-        prefix: 'fa'
-      });
-
-      let marker = L.marker([lat, lng], { icon: mark })
-                    .bindPopup(`<h2>${sna}</h2>@${sarea} x ${tot}`);
-
-      markers.push(marker);
+    let clusterGroup = new L.MarkerClusterGroup({
+        disableClusteringAtZoom: 15,
+        maxClusterRadius: zoom => {
+            if (zoom <= 11) return 80;
+            if (zoom <= 12) return 70;
+            if (zoom <= 14) return 60;
+            return 50;
+        }
     });
 
-  clusterGroup.addLayers(markers);
-  map.addLayer(clusterGroup);
+    let markers = [];
+
+    csv
+        .split('\n').slice(1)
+        .map(line => line.split(','))
+        .forEach(arr => {
+            let [sno, sna, tot, lat, lng, sarea] = arr;
+
+            let mark = L.ExtraMarkers.icon({
+                icon: 'fa-bicycle',
+                markerColor: 'blue',
+                shape: 'square',
+                prefix: 'fa'
+            });
+
+            let marker = L.marker([lat, lng], { icon: mark })
+                .bindPopup(`<h2>${sna}</h2>@${sarea} x ${tot}`);
+
+            markers.push(marker);
+        });
+
+    clusterGroup.addLayers(markers);
+    map.addLayer(clusterGroup);
 }
 
 parseCSV(csv1);
@@ -85,43 +88,97 @@ window.newWallet = [];
 
 
 
-function newAddress () { // (password) {
-  if (typeof keystore.getHexAddress !== 'function') {
-    return false
-  }
-
-  let wallet = keystore
-
-  privateKey = wallet.getHexPrivateKey()
-  address = wallet.getHexAddress(true)
-  newWallet = [address, privateKey];
-  /*
-  wallet.toV3String(password, {}, (err, v3Json) => {
-    if (err) {
-      console.warn(err.message)
-      return
-    }
-    keystoreJson = v3Json
-    keystoreJsonDataLink = encodeURI('data:application/json;charset=utf-8,' + keystoreJson)
-    fileName = `${wallet.getV3Filename()}.json`
-  })
-  */
-}
-
-function generate () {
-
-  let wallet = yoethwallet.wallet
-
-  wallet.generate('', hdPathString, (err, keystore_f) => {
-    if (err) {
-      console.warn(err.message)
-      return
+function newAddress() { // (password) {
+    if (typeof keystore.getHexAddress !== 'function') {
+        return false
     }
 
-    keystore = keystore_f
-    newAddress() // (password)
-  })
+    let wallet = keystore
+
+    privateKey = wallet.getHexPrivateKey()
+    address = wallet.getHexAddress(true)
+    newWallet = [address, privateKey];
+    /*
+    wallet.toV3String(password, {}, (err, v3Json) => {
+      if (err) {
+        console.warn(err.message)
+        return
+      }
+      keystoreJson = v3Json
+      keystoreJsonDataLink = encodeURI('data:application/json;charset=utf-8,' + keystoreJson)
+      fileName = `${wallet.getV3Filename()}.json`
+    })
+    */
+}
+
+function generate() {
+
+    let wallet = yoethwallet.wallet
+
+    wallet.generate('', hdPathString, (err, keystore_f) => {
+        if (err) {
+            console.warn(err.message)
+            return
+        }
+
+        keystore = keystore_f
+        newAddress() // (password)
+    })
 
 }
+
+
+
+function addressToLatLng(addr) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+        "address": addr
+    }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results[0].geometry.location.lat() + "," + results[0].geometry.location.lng() + "\n");
+        } else {
+            console.log(addr + "查無經緯度" + "\n");
+        }
+    });
+}
+
+
+$('#toolbar .hamburger').on('click', function() {
+    $(this).parent().toggleClass('open');
+});
+
+$(function() {
+    if (localStorage.youbike_wallet) {
+        $('#address').text(JSON.parse(localStorage.youbike_wallet)[0]);
+        $('#private_key').text(JSON.parse(localStorage.youbike_wallet)[1]);
+        $('#new_wallet').hide();
+        $('#remove_wallet').show();
+    }
+
+});
+
+$('#new_wallet').on('click', function() {
+    generate();
+    $('#address').text(newWallet[0]);
+    $('#private_key').text(newWallet[1]);
+    localStorage.youbike_wallet = JSON.stringify(newWallet);
+    $('#new_wallet').hide();
+    $('#remove_wallet').show();
+});
+
+$('#remove_wallet').on('click', function() {
+    generate();
+    $('#address').text("");
+    $('#private_key').text("");
+    localStorage.youbike_wallet = "";
+    $('#new_wallet').show();
+    $('#remove_wallet').hide();
+});
+
+$('#locate').on('click', function() {
+    geolocate();
+});
+
 
 window.generate = generate;
+window.addressToLatLng = addressToLatLng;
